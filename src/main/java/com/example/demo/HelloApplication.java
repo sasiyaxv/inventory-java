215996e4dfef8b11707ac905d03687dc0330f737
sashminda iranga <sashmindaps4@gmail.com>
2022-11-09 14:04:49 +0530

    test

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
-        stage.setTitle("Inventory");
+        stage.setTitle("Inventory-sliit");
         stage.setScene(scene);
         stage.show();
     }
