c7353daf7b69f838556b708fab8213e838085dd3
sashminda iranga <sashmindaps4@gmail.com>
2022-12-11 15:10:57 +0530

    updated screen sizes

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -10,7 +11,11 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1550, 1080);
+
+        double height=  Screen.getPrimary().getBounds().getHeight();
+        double width=   Screen.getPrimary().getBounds().getWidth();
+
+        Scene scene = new Scene(fxmlLoader.load(), width, height);
         stage.setTitle("Tissue culture laboratory inventory");
         stage.setScene(scene);
         stage.show();
